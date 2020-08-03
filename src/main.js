538dddf7f7a424ea0f48bdd020dfa17a1bca95d6
jiaoshou <731550534@qq.com>
2020-08-03 15:42:33 +0800

    asd

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// import NProgress from 'nprogress'
+// import 'nprogress/nprogress.css'
+import "./assets/index.css";
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+// Vue.component(Button.name, Button);
+// Vue.component(Select.name, Select);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 输出文件目录
+  outputDir: 'docs',
+}
